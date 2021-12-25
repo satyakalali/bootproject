ef15ee83e7d63385f2319cc6acb958d575f5aef8
satya <satyakalali87@gmail.com>
2021-12-25 16:48:39 +0530

    Added applicaiton yml file

--- src/main/java/com/boot/project/bike/VogoBike.java
+++ src/main/java/com/boot/project/bike/VogoBike.java
@@ -1,11 +1,13 @@
 package com.boot.project.bike;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
 @Component
 @Data
+@ConfigurationProperties
 public class VogoBike {
 
 	private int vogo_id;
