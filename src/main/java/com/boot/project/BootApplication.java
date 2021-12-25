ceac610b37176eeffccc99828a72129fb12df579
satya <satyakalali87@gmail.com>
2021-12-25 11:16:09 +0530

    modified vogo bike

--- src/main/java/com/boot/project/BootApplication.java
+++ src/main/java/com/boot/project/BootApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.boot.project.bike.VogoBike;
+
+
 @SpringBootApplication
 public class BootApplication {
 
--- src/main/java/com/boot/project/bike/VogoBike.java
+++ src/main/java/com/boot/project/bike/VogoBike.java
@@ -1,4 +1,4 @@
-package com.boot.project;
+package com.boot.project.bike;
 
 import org.springframework.stereotype.Component;
 
